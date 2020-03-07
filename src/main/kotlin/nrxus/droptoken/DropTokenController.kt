3a79ccad2898298866a640229aefc84a45be060b
Andres Medina <andresnrx@gmail.com>
2020-03-07 11:13:49 -0800

    extract root api path to top of the controller

--- src/main/kotlin/nrxus/droptoken/DropTokenController.kt
+++ src/main/kotlin/nrxus/droptoken/DropTokenController.kt
@@ -1,18 +1,16 @@
 package nrxus.droptoken
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
+@RequestMapping("/drop_token")
 class DropTokenController(private val repository: DropTokenRepository) {
-    @GetMapping("/drop_token")
+    @GetMapping
     fun getGames(): GamesResponse =
             GamesResponse(repository.findAll().map { it.id.toString() })
 
-    @PostMapping("/drop_token")
+    @PostMapping
     fun newGame(@Valid @RequestBody request: NewGameRequest): GameResponse {
         val dropToken = repository.save(DropToken())
         return GameResponse(dropToken.id.toString())
